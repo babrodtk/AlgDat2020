96e679bd7399656439ef043cf04c9bb50b4ebd5d
babrodtk <andre.r.brodtkorb@gmail.com>
2020-08-17 09:14:05 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Teller til ti");
+        for (int i=1; i<=10; ++i) {
+            System.out.println(i);
+        }
+    }
+}
