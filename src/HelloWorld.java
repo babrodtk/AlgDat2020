3bd5429b7483d910de4d450713e1884a1e2a31a8
babrodtk <andre.r.brodtkorb@gmail.com>
2020-08-17 09:23:36 +0200

    Petra kan telle til 20.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,5 +6,10 @@ public class HelloWorld {
         for (int i=1; i<=10; ++i) {
             System.out.println(i);
         }
+
+        System.out.println("Petra teller til tyve");
+        for (int i=11; i<=20; ++i) {
+            System.out.println(i);
+        }
     }
 }
