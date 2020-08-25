5b9082f48f62d0992a06bd075f80f85b263e468e
babrodtk <andre.r.brodtkorb@gmail.com>
2020-08-25 15:13:00 +0200

    Updated lecture code to be more complete example.

--- src/BTNode.java
+++ src/BTNode.java
@@ -6,8 +6,29 @@ public class BTNode {
         Node right_child;
         char value;
 
-        public Node(char value) {
+        public Node(Node parent, char value) {
+            this.parent = parent;
             this.value = value;
+            this.left_child = null;
+            this.right_child = null;
+        }
+
+        void addLeftChild(char value) {
+            this.left_child = new Node(this, value);
+        }
+
+        void addRightChild(char value) {
+            this.right_child = new Node(this, value);
+        }
+
+        void print() {
+            System.out.println(this.value);
+            if (this.left_child != null) {
+                this.left_child.print();
+            }
+            if (this.right_child != null) {
+                this.right_child.print();
+            }
         }
 
         Node grandParent() {
@@ -18,14 +39,18 @@ public class BTNode {
     public static void main(String[] args) {
         char[] a = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
 
-        Node root = new Node(a[0]);
-        root.left_child = new Node(a[1]);
-        root.right_child = new Node(a[2]);
+        Node root = new Node(null, a[0]);
+        root.addLeftChild(a[1]);
+        root.addRightChild(a[2]);
 
-        root.left_child.left_child = new Node(a[3]);
-        root.left_child.right_child = new Node(a[4]);
+        root.left_child.addLeftChild(a[3]);
+        root.left_child.addRightChild(a[4]);
 
-        root.right_child.left_child = new Node(a[5]);
-        root.right_child.right_child = new Node(a[6]);
+        root.right_child.addLeftChild(a[5]);
+        root.right_child.addRightChild(a[6]);
+
+        System.out.println("Printing binary tree!");
+        //Why do we print it in this order?
+        root.print();
     }
 }
